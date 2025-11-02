7436c475f921ac41339359cc6094eedc4d471345
Satyajeetsinh007 <satyajadeja52@gmail.com>
2025-11-02 10:54:42 +0530

    Done same thing with promise chaining

--- api.js
+++ api.js
@@ -3,14 +3,24 @@ const descPara=document.querySelector("#Description")
 const button=document.querySelector("#Btn")
 
 
-getBreeds=async()=>{
-    console.log("Getting data.....")
-    let response=await fetch(URL)
-    console.log("Hi")
-    console.log(response)
-    let response2=await response.json()
-    console.log("Hi2")
-    console.log(response2.data[0].attributes.description)
-    descPara.innerText=response2.data[0].attributes.description
+// getBreeds=async()=>{
+//     console.log("Getting data.....")
+//     let response=await fetch(URL)
+//     console.log("Hi")
+//     console.log(response)
+//     let response2=await response.json()
+//     console.log("Hi2")
+//     console.log(response2.data[0].attributes.description)
+//     descPara.innerText=response2.data[0].attributes.description
+// }
+
+function getBreeds(){
+    fetch(URL).then((response)=>{
+        return response.json()
+    })
+    .then((response2)=>{
+        console.log(response2)
+        descPara.innerText=response2.data[0].attributes.description
+    })
 }
 button.addEventListener("click",getBreeds)
