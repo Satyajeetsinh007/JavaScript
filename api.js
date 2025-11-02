1bb0fe0e112ad824c987db75b31993e3e95066e8
Satyajeetsinh007 <satyajadeja52@gmail.com>
2025-11-02 10:49:45 +0530

    Added button further

--- api.js
+++ api.js
@@ -1,5 +1,8 @@
 const URL="https://dogapi.dog/api/v2/breeds";
 const descPara=document.querySelector("#Description")
+const button=document.querySelector("#Btn")
+
+
 getBreeds=async()=>{
     console.log("Getting data.....")
     let response=await fetch(URL)
@@ -10,3 +13,4 @@ getBreeds=async()=>{
     console.log(response2.data[0].attributes.description)
     descPara.innerText=response2.data[0].attributes.description
 }
+button.addEventListener("click",getBreeds)
