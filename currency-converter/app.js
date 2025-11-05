12954c148abd7160134b75633583fc812983d611
Satyajeetsinh007 <satyajadeja52@gmail.com>
2025-11-05 18:03:42 +0530

    done few things to improve otherwise completed

--- currency-converter/app.js
+++ currency-converter/app.js
@@ -1,6 +1,16 @@
-const BASE_URL="https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${fromcurrency}.json"
+const BASE_URL="https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies"
 
 const dropdowns=document.querySelectorAll(".dropdown select")
+
+const button=document.querySelector(".btn")
+
+const toCurr=document.querySelector(".to select")
+
+const fromCurr=document.querySelector(".from select")
+
+const msg=document.querySelector(".msg")
+
+let amount=document.querySelector("form input")
 
 
 
@@ -21,14 +31,43 @@ for(let select of dropdowns){
     
     select.addEventListener("change",(evt)=>{
         updateFlag(evt.target)
+
     })
 }
 const updateFlag=(element)=>{
+    // console.log(element)
     let currCode=element.value
     let countryCode=countryList[currCode]
-    console.log(currCode,countryCode)
+    // console.log(currCode,countryCode)
     let newSrc=`https://flagsapi.com/${countryCode}/shiny/64.png`
     let img=element.parentElement.querySelector("img")
     img.src=newSrc
 
     }
+button.addEventListener("click",async(evt)=>{
+    evt.preventDefault();
+    let amtVal=amount.value;
+     // note .value propery always changes when user types but if U do amount.getAttribute("value") it will still show 100
+    if(amtVal===""||amtVal<1){
+        amtVal=1
+        amount.value="1"
+    }
+    getExchangeRate(amtVal);
+    // msg.innerText=`${evt.}`
+})
+
+
+
+async function getExchangeRate(amtVal) {
+  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+  const response = await fetch(URL);
+  
+  const data = await response.json();
+  let rate=data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+
+  let finalAmt=rate*amtVal;
+  msg.innerText=`${amtVal} ${dropdowns[0].value}=${finalAmt} ${dropdowns[1].value}`
+
+}
+
+
